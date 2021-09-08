69cd2b204a43252c36ea0e385500588b85a53519
Vei Yie <veiyie1998@gmail.com>
2021-09-08 11:07:24 +0800

    Updates database and routes signup file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config
 const express = require('express')
 const morgan = require('morgan')
 const signupRouter = require('./routes/signup')
@@ -8,7 +9,14 @@ const PORT = process.env.PORT || 3000
 // body parser
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+console.log(process.env.PORT)
 
 // view engine - IGNORED FOR TODAY
+
+// route middleware
+app.use('/signup', signupRouoter)
+app.get('login', (req, res) => {
+    res.send(req.query.message)
+})
 
 app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}`))
