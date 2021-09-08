bffa789e28301dbcacf5b34b8b9b9f9e7bc3416b
Vei Yie <veiyie1998@gmail.com>
2021-09-08 10:14:24 +0800

    Add sql scripts for db and tables

---
+++ sql/create_db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS incode5_fk;
+
+CREATE DATABASE incode5_fk
+-- uuid 
---
+++ sql/seed_tables.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (first_name, last_name, email, password)
+
+VALUES 
+    ('Vei Yie', 'Tan', 'veiyie1998@gmail.com', 'hf97qyr983y98'),
+    ('Jenny', 'Lee', 'marui@gmail.com', 'hf97qyr983y98'),
+    ('Sphagetti', 'Soo', 'huhuigiyeifw@gmail.com', '2163278blj'),
+    ('Vincenzo', 'Soong', 'vincenzo@gmail.com', '73r2t7yuuyg');
+
+INSERT INTO posts (user_id, title, content)
+
+VALUES 
+    (1, 'baking recipes', 'making cakes'),
+    (2, 'baking recipes', 'making cakes'),
+    (3, 'baking recipes', 'making cakes'),
+    (4, 'baking recipes', 'making cakes');
